0595871b4fbd7ed436fbea5aad228dd136fc0242
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2021-11-21 16:38:16 +0300

    draft created

---
+++ js/signup.js
@@ -0,0 +1,4 @@
+const loading = document.getElementById("loading");
+const submitBtn = document.getElementById("submit");
+const email = document.querySelector("email");
+const password = document.querySelector("password");
